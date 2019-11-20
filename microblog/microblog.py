fbc1e12dfbc165aeb006346a073b9a58ddc992e9
buscaroli <m.buscaroli@outlook.com>
2019-11-20 19:32:00 +0000

    microblog, added shell context: start of Chapter 5

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,1 +1,11 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+## We are adding some context to the flask shell so that
+## if we run flask shell, we can start hacking around with
+## all the required variables in memory
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+
