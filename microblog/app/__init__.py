06c2f6cd3533365d1da1ce60ec11a7260d0d915b
buscaroli <m.buscaroli@outlook.com>
2020-01-04 15:37:22 +0000

    Added Login and registration, made a small error so had to look at author's routes.py to find out the problem

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -2,10 +2,15 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
+# The following imposts need to be at the end of the file to
+# avoid 'recursive imports'
 from app import routes, models
--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,10 +1,20 @@
 from datetime import datetime
-from app import db
+from app import db, login
+# werkzeug takes care of the hashing used in the password_hash field
+# in the User class.
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-## The class User inherits from the SQLAlchemy class db.Model
-class User(db.Model):
-    ## Each field is created as an instance of the class db.Column
+# Necessary for Flask-Login in order to be able to load a user
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+# The class User inherits from the SQLAlchemy class db.Model
+class User(UserMixin, db.Model):
+    # Each field is created as an instance of the class db.Column
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -16,12 +26,18 @@ class User(db.Model):
         if given the username 'susan' it would print <User susan>'''
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Post(db.Model):
-    ## The timestamp field is indexed so that we can retrieve
-    ## posts in chronological order
-    ## The user_is field is initialised as a foreign key to user.id
-    ##in order to reference an id value from the user table
+    # The timestamp field is indexed so that we can retrieve
+    # posts in chronological order
+    # The user_is field is initialised as a foreign key to user.id
+    # in order to reference an id value from the user table
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
